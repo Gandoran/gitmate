b69a014760f4203712c0c270db53db98d846889d
gandoran <giakicarloni@gmail.com>
2026-03-27 21:32:19 +0100

    feat: aggiunto funzionalità di gestione del commit in `git_extractor.py` e integrazione nel processo di creazione del messaggio di commit in `orchestrator.py`

--- git_extractor.py
+++ git_extractor.py
@@ -6,4 +6,29 @@ def extract_changes():
         result = subprocess.run(args=["git", "diff", "--staged"],check=True, capture_output=True, text=True)
         return result.stdout
     except subprocess.CalledProcessError:
-        print("Not in a git folder: ")
+        return None
+
+def git_add():
+    try:
+        subprocess.run(args=['git','add','.'],check=True)
+    except subprocess.CalledProcessError:
+        return("No File to add")
+
+def git_commit(message):
+    try:
+        subprocess.run(args=['git','commit','-m',message],check=True)
+    except subprocess.CalledProcessError:
+        return ("Cannot commit")
+
+def git_retrieve_current_branch():
+    try:
+        result = subprocess.run(args=["git","rev-parse","--abbrev-ref","HEAD"],check=True, capture_output=True, text=True)
+        return result.stdout.strip()
+    except subprocess.CalledProcessError:
+        return("Cannot retrieve a branch")
+    
+def git_push_commit():
+    try:
+        subprocess.run(args=['git','push','origin',git_retrieve_current_branch()])
+    except subprocess.CalledProcessError:
+        return("Cannot push")
--- orchestrator.py
+++ orchestrator.py
@@ -3,8 +3,14 @@ import ia_chat
 import istructions
 
 def create_commit_message():
+    git_extractor.git_add()
     changes = git_extractor.extract_changes()
     if(not changes):
         return "Nothing added to the commit"
     message = ia_chat.ollama_chat(istructions.REGOLE_COMMIT,changes)
+    print(f"\n Messaggio generato:\n\n{message}\n")
+    response = input("Vuoi procedere? (Y/N): ")
+    if(response == 'Y' or response == 'y'):
+        git_extractor.git_commit(message)
+        git_extractor.git_push_commit()
     return message
